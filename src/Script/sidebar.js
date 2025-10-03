7bb44891a03e2fa9fae4463acb972a7415e1a526
MayGahaRy <karamiab85@gmail.com>
2025-10-03 18:24:50 +0330

    SideBarOpenCloseSystem_addfont

---
+++ src/Script/sidebar.js
@@ -0,0 +1,9 @@
+var sidebar = document.getElementById("sidebar-js");
+
+function toggleSidebar() {
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+}
